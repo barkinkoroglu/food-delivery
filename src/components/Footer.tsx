920f6aaf1b7c8510a8717b32ad215f4c98580a75
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-07-27 15:08:46 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-lg">
+        BARKIN
+      </Link>
+      <p>© ALL RIGHTS RESERVED</p>
+    </footer>
+  );
 }
 
 export default Footer;
